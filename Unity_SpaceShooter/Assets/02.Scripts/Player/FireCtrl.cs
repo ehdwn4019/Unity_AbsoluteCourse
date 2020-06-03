00381be96823f4d850ee15711d35b75b653d5faa
ehdwn4019 <ehdwn7531@gmail.com>
2020-06-03 17:42:33 +0900

    -총알 발사 궤적 생성 -총기와 드럼통 파티클 시스템 생성 -드럼통 폭발 반경 생성 -총기,드럼통 사운드 생성 -에너미 생성 -에너미 애니매이션 생성 -내비게이션 생성중

---
+++ Unity_SpaceShooter/Assets/02.Scripts/Player/FireCtrl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerSfx
+{
+    public AudioClip[] fire;
+    public AudioClip[] reload;
+}
+
+public class FireCtrl : MonoBehaviour
+{
+    public enum WeaponType
+    {
+        RIFLE=0,
+        SHOTGUN
+    }
+
+    public WeaponType currWeapon = WeaponType.RIFLE;
+
+    public GameObject bullet;
+    public ParticleSystem cartridge;
+    private ParticleSystem muzzleFlash;
+    private AudioSource _audio;
+    public Transform firePos;
+    public PlayerSfx playerSfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        muzzleFlash = firePos.GetComponentInChildren<ParticleSystem>();
+        _audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        Instantiate(bullet, firePos.position, firePos.rotation);
+        cartridge.Play();
+        muzzleFlash.Play();
+        FireSfx();
+    }
+
+    private void FireSfx()
+    {
+        var _sfx = playerSfx.fire[(int)currWeapon];
+        _audio.PlayOneShot(_sfx, 1.0f);
+    }
+}
